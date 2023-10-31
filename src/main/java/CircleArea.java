5f531a3ebd449d8a5c118161e361376de5a6f676
TrueRaisins <idanik.ishot@gmail.com>
2023-10-31 18:04:18 +0300

    initial commit

--- src/main/java/CircleArea.java
+++ src/main/java/CircleArea.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class CircleArea {
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius = 4.2;
+        double pi = Math.PI;
+        double area = pi * radius * radius;
+
+        System.out.println("Area is: " + area);
+    }
 }
